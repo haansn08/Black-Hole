fb6d8e91a29b1ad6264f368de7125600aaff64f1
Stefan Haan <stefanhaan08@gmail.com>
2016-03-20 23:02:04 +0100

    made positions selectable

--- app/src/main/java/com/shaan/blackhole/GameView.java
+++ app/src/main/java/com/shaan/blackhole/GameView.java
@@ -3,20 +3,30 @@ package com.shaan.blackhole;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by stefan on 20/03/16.
  */
 public class GameView extends View {
     private final Paint bluePaint;
+    private final Paint blueSelectionPaint;
     private final Paint blackLinePaint;
     private final Paint redPaint;
     private final Paint blackPaint;
+
     private Game game;
+    private float radius;
+
+    private PointF lastTouch = new PointF();
+    private int selectedL, selectedR;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -36,44 +46,65 @@ public class GameView extends View {
         blackPaint = new Paint();
         blackPaint.setColor(Color.BLACK);
         blackPaint.setAlpha(100);
+
+        blueSelectionPaint = new Paint(blackLinePaint);
+        blueSelectionPaint.setColor(Color.BLUE);
+        blueSelectionPaint.setAlpha(50);
+        blueSelectionPaint.setStrokeWidth(50f);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         GameField gameField = game.getField();
+        selectedL = -1;
+        selectedR = -1;
 
-        int lines = game.getFieldSize();
-        float width = canvas.getWidth();
-        float radius = width / (2*lines + 2);
-
-        canvas.translate(width / 2 - radius, 0);
-        for (int l = 0; l < lines; l++) {
-            canvas.translate(
-                    -(2*l + 1) * radius,
-                    (float) (Math.sqrt(3) * radius)
-            );
+        radius = canvas.getWidth() / (2*game.getFieldSize() + 2); //Calculate raduis for circles
+        float cx, cy; //Center for each circle
+        cx = canvas.getWidth() / 2 - radius;
+        cy = 0;
+        for (int l = 0; l < game.getFieldSize(); l++) {
+            cx -= (2*l + 1) * radius;
+            cy += (float) (Math.sqrt(3) * radius);
             for (int r = 0; r <= l; r++) {
-                canvas.translate(2 * radius, 0);
-                canvas.drawCircle(0, 0, radius, blackLinePaint); //draw border
+                cx += 2 * radius;
 
-                if (gameField.getFieldNumber(l, r) == 0)
-                    canvas.drawCircle(0, 0, radius, blackPaint);
-                else {
-                    Paint playerPaint = gameField.getFieldPlayer(l, r) == 0 ? redPaint : bluePaint;
-                    playerPaint.setAlpha(50);
-                    canvas.drawCircle(0, 0, radius, playerPaint);
-                    playerPaint.setAlpha(255);
+                canvas.save();
+                canvas.translate(cx, cy);
 
-                    String number = gameField.getFieldNumber(l, r) + "";
-                    canvas.drawText(
-                            number,
-                            -playerPaint.measureText(number) / 2,
-                            radius / 2,
-                            playerPaint
-                    );
+                drawGameCircle(canvas, l, r);
+                float distanceToTouch = PointF.length(lastTouch.x - cx, lastTouch.y - cy);
+                if (distanceToTouch < radius) {
+                    canvas.drawCircle(0, 0, radius, blueSelectionPaint);
+                    selectedL = l;
+                    selectedR = r;
                 }
+
+                canvas.restore();
             }
+        }
+    }
+
+    private void drawGameCircle(Canvas canvas, int l, int r) {
+        GameField gameField = game.getField();
+        canvas.drawCircle(0, 0, radius, blackLinePaint); //draw border
+
+        if (gameField.getFieldNumber(l, r) == 0)
+            canvas.drawCircle(0, 0, radius, blackPaint);
+        else {
+            Paint playerPaint = gameField.getFieldPlayer(l, r) == 0 ? redPaint : bluePaint;
+            playerPaint.setAlpha(50);
+            canvas.drawCircle(0, 0, radius, playerPaint);
+            playerPaint.setAlpha(255);
+
+            String number = gameField.getFieldNumber(l, r) + "";
+            canvas.drawText(
+                    number,
+                    -playerPaint.measureText(number) / 2,
+                    radius / 2,
+                    playerPaint
+            );
         }
     }
 
@@ -83,7 +114,11 @@ public class GameView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
-        return super.onTouchEvent(event);
+        lastTouch = new PointF(event.getX(), event.getY());
+        if(event.getAction() == MotionEvent.ACTION_UP && selectedL != -1 && selectedR != -1){
+            Toast.makeText(getContext(), "R"+selectedR+"L"+selectedL, Toast.LENGTH_SHORT).show();
+        }
+        invalidate();
+        return true;
     }
 }
